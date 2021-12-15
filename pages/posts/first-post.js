d79f401d10608219cb0376c7d174caea2de94516
조민혁 <chohyuk0725@gmail.com>
2021-12-15 09:28:50 +0900

    네비게이션

---
+++ pages/posts/first-post.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const Firstpost = () => {
+  return (
+    <div>
+      <h1>first post</h1>
+      <h2>
+        <Link href="/">backHome</Link>
+      </h2>
+    </div>
+  );
+};
+
+export default Firstpost;
